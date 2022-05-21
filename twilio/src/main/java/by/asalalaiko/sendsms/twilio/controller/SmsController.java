2125f12faf29f95531b04c6be7d6b9c380e9a245
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2022-05-21 23:20:44 +0300

    2th commit

--- twilio/src/main/java/by/asalalaiko/sendsms/twilio/controller/SmsController.java
+++ twilio/src/main/java/by/asalalaiko/sendsms/twilio/controller/SmsController.java
@@ -5,20 +5,29 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SmsController {
+
+    @Value("${twilio.account.sid}")
+    private  String username;
+    @Value("${twilio.auth.token}")
+    private  String password;
+    @Value("${twilio.phonenumber.from}")
+    private  String phonenumberFrom;
+
     @GetMapping(value = "/sendSMS")
-    public ResponseEntity<String> sendSMS() {
+    public ResponseEntity<String> sendSMS(@RequestParam(required = true) String phone, @RequestParam(required = true) String code) {
 
-        Twilio.init("AC5a3e5e089e068915532b3fb3a88ef550", "011bde7006a5ab4126a2ecca5c32b620");
+        Twilio.init(username, password);
 
-        Message.creator(new PhoneNumber("+375297832572"),
-                new PhoneNumber("+19207813784"), "Hello from Twilio 📞").create();
+        Message.creator(new PhoneNumber("+"+phone), new PhoneNumber(phonenumberFrom), "Your code: " + code).create();
 
         return new ResponseEntity<String>("Message sent successfully", HttpStatus.OK);
     }
